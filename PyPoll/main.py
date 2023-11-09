03a92731ff1f54599a5ff8f6f6165ad6261ca739
AlanBigData <alawrence026@gmail.com>
2023-11-08 20:25:55 -0500

    commit to main

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,4 +1,18 @@
 # PyPoll mainfile
+# Modules
+import pandas as pd
 
-# def pypoll():
-#     test
+# Define all of the variables for calculated values
+df = pd.read_csv(r'Resources\election_data.csv')
+
+print(df.head())
+
+total_votes = df['Ballot ID'].count()
+candidates = df['Candidate'].unique()
+print(total_votes)
+
+print(candidates)
+candidate_votes = df['Candidate'].value_counts()
+print(candidate_votes)
+
+# Calculate the total number of months using pandas df
